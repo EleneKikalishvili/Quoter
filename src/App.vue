7e0aefa121a927bb6fe24ccd13d225681195134f
EleneKikalishvili <elene.kikalishvili.1@btu.edu.ge>
2020-03-19 00:01:42 +0400

    imported Header component, added conditional in newQuote function to check quotes list length, added style for container to fix info box at bottom

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="container">
+    <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
     <app-new-quote @quoteAdded="newQuote"></app-new-quote>
     <app-quote-grid :quotes="quotes" @quoteDeleted="delQuote"></app-quote-grid>
-    <div class="row">
+    <div class="row fixed">
       <div class="col-sm-12 text-center">
-        <div class="alert alert-info">Info: Click on a Quote to Delete it!</div>
+        <div class="alert alert-info">Info: Click on a Quote to Delete it !</div>
       </div>
     </div>
   </div>
@@ -13,15 +14,19 @@
 <script>
 import QuoteGrid from "./components/QuoteGrid";
 import NewQuote from "./components/NewQuote";
+import Header from "./components/Header";
 export default {
   data: function() {
     return {
-      quotes: ["just a Quote"],
+      quotes: ["just a Quote to see something"],
       maxQuotes: 10
     };
   },
   methods: {
     newQuote(quote) {
+      if (this.quotes.length >= this.maxQuotes) {
+        return alert("DELETE QUOTES FIRST");
+      }
       this.quotes.push(quote);
     },
     delQuote(index) {
@@ -30,10 +35,19 @@ export default {
   },
   components: {
     appQuoteGrid: QuoteGrid,
-    appNewQuote: NewQuote
+    appNewQuote: NewQuote,
+    appHeader: Header
   }
 };
 </script>
 
-<style>
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+.fixed {
+  margin-top: auto;
+}
 </style>
